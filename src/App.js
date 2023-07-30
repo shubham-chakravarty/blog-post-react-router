03f74aeebf731803f4df47000de6c17a899330e8
shubham-chakravarty <shubham.chakravarty@pdgc.com>
2023-07-30 22:42:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react'; 
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
